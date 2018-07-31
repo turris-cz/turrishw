48c9dde199a9f6eb6040411b93922c442ff20db1
Karel Kočí <karel.koci@nic.cz>
2018-07-31 10:13:00 +0200

    Few additional fixes and cleanups for dash

--- tests_roots/swconfig.common.sh
+++ tests_roots/swconfig.common.sh
@@ -1,21 +1,15 @@
 # Common shell implementation of switch config
 set -e
 
-if [ "$1" == "list" ]; then
-	if [ $# -eq 1 ]; then
-		exit 1
-	fi
+if [ "$1" = "list" ]; then
+	[ $# -eq 1 ]
 	list
-elif [ "$1" == "dev" ]; then
-	if [ "$2" != "switch0" ]; then
-		exit 1
-	fi
-	if [ "$3" == "show" ]; then
+elif [ "$1" = "dev" ]; then
+	[ "$2" = "switch0" ]
+	if [ "$3" = "show" ]; then
 		show_all
 	else
-		if [ "$3" != "port" -o "$5" != "show" -o $# -ne 5 ]; then
-			exit 1
-		fi
+		[ "$3" = "port" -a "$5" = "show" -a $# -eq 5 ]
 		show_port "$4"
 	fi
 else
