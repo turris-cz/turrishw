fef2fc672e572bc7a9311ae2da10e43aa9a8edc3
Karel Kočí <karel.koci@nic.cz>
2018-07-31 10:09:53 +0200

    Fix swconfig mockup for dash interpreter

--- tests_roots/swconfig.common.sh
+++ tests_roots/swconfig.common.sh
@@ -2,15 +2,20 @@
 set -e
 
 if [ "$1" == "list" ]; then
-	[ $# -eq 1 ]
+	if [ $# -eq 1 ]; then
+		exit 1
+	fi
 	list
 elif [ "$1" == "dev" ]; then
-	[ "$2" == "switch0" ]
+	if [ "$2" != "switch0" ]; then
+		exit 1
+	fi
 	if [ "$3" == "show" ]; then
 		show_all
 	else
-		[ "$3" == "port" ]
-		[ "$5" == "show" ]
+		if [ "$3" != "port" -o "$5" != "show" -o $# -ne 5 ]; then
+			exit 1
+		fi
 		show_port "$4"
 	fi
 else
