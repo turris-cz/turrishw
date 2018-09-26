d051679ab9757f5949476071ec86d726b8e53e26
Martin Petráček <martin.petracek@nic.cz>
2018-09-26 14:51:23 +0200

    add test script

--- thw.py
+++ thw.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import turrishw
 import json
-import pprint
 import sys
 
-turrishw.__P_ROOT__=sys.argv[1]
-print(json.dumps(turrishw.get_ifaces()))
+if len(sys.argv) > 1:
+    turrishw.__P_ROOT__=sys.argv[1]
+print(json.dumps(turrishw.get_ifaces(), indent=2, sort_keys=True))
