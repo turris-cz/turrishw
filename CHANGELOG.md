341279562701eac344cdc2299c91cae6f1046c20
Martin Matějek <martin.matejek@nic.cz>
2022-04-26 15:45:21 +0200

    mox: get eth interface name from label instead of port id
    Read `of_node/label` instead of `phys_switch_id` and don't try to get
    port label from "LAN" + port_id.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Changed
+* Mox + Omnia: Get LAN & SFP interface name from label instead of port id
+
 ## [0.5.3] - 2022-04-28
 ### Fixed
 * Fix interface labels mismatch (LAN1=lan5, ..., LAN5=lan1) on Turris 1.x,
--- turrishw/mox.py
+++ turrishw/mox.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2021, CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (c) 2018-2022, CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -22,9 +22,10 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import logging
 import os
 import re
-import logging
+
 from . import utils
 
 logger = logging.getLogger(__name__)
@@ -38,8 +39,8 @@ def _get_modules():
 
 
 def get_interfaces():
-    def append_iface(iface, if_type, bus, module_seq, port, macaddr):
-        ifaces.append(utils.iface_info(iface, if_type, bus, module_seq, str(port), macaddr))
+    def append_iface(iface: str, if_type: str, bus: str, module_seq: int, port_label: str, macaddr: str):
+        ifaces.append(utils.iface_info(iface, if_type, bus, module_seq, port_label, macaddr))
 
     def get_module_rank(name):
         seq = [i + 1 for i, s in enumerate(modules) if name in s]
@@ -57,22 +58,20 @@ def get_interfaces():
         macaddr = utils.get_first_line(os.path.join(iface_path, "address")).strip()
         if "d0032004.mdio-mii" in path:
             # MDIO bus on MOXTET - for switches
-            port = int(utils.get_first_line(os.path.join(iface_path, "phys_port_name"))[1:])
-            # phys_port_name is "p{number}", e.g. 'p1' - remove leading p and
-            # convert it to int
-            switch = int(utils.get_first_line(os.path.join(iface_path, "phys_switch_id"))[0:2])
+            port_label = utils.get_iface_label(iface_path)
+            switch_id = int(utils.get_first_line(os.path.join(iface_path, "phys_switch_id"))[0:2])
             # phys_switch_id is 0{sequence_num}000000, e.g. 00000000 for 1st
             # switch, 01000000 for 2nd and so on. Don't know why.
             # take just first 2 letters and convert to int
-            switch = switch_idxs[switch]
-            if port != 10:
-                append_iface(iface, "eth", "eth", switch, port - 1, macaddr)
-            else:  # SFP is announced as "p10"
+            switch_no = switch_idxs[switch_id]
+            if port_label == "SFP":
                 sfp_seq = get_module_rank("sfp")
-                append_iface(iface, "eth", "sfp", sfp_seq, 0, macaddr)
+                append_iface(iface, "eth", "sfp", sfp_seq, port_label, macaddr)
+            else:  # everything else should be ethernet
+                append_iface(iface, "eth", "eth", switch_no, port_label, macaddr)
         elif "d0030000.ethernet" in path:
             # ethernet port on the CPU board
-            append_iface(iface, "eth", "eth", 0, 0, macaddr)
+            append_iface(iface, "eth", "eth", 0, "ETH0", macaddr)
         elif "d0040000.ethernet" in path:
             # ethernet on the MOXTET connector
             # when some switches are connected, it shouldn't be touched (it's
@@ -80,10 +79,10 @@ def get_interfaces():
             # actually the SFP interface
             sfp_seq = get_module_rank("sfp")
             if not switch_idxs and sfp_seq:
-                append_iface(iface, "eth", "sfp", sfp_seq, 0, macaddr)
+                append_iface(iface, "eth", "sfp", sfp_seq, "SFP", macaddr)
         elif "d00d0000.sdhci" in path:
             # SDIO on the CPU board
-            append_iface(iface, "wifi", "sdio", 0, 0, macaddr)
+            append_iface(iface, "wifi", "sdio", 0, "0", macaddr)
         elif "d0070000.pcie" in path:
             # PCIe on the MOXTET connector
             # can be PCI (B) or USB3.0 (F) module
@@ -97,15 +96,15 @@ def get_interfaces():
                     logger.warning("unknown port on USB3.0 module")
             else:  # PCI module
                 pci_seq = get_module_rank("pci")
-                append_iface(iface, utils.find_iface_type(iface), "pci", pci_seq, 0, macaddr)
+                append_iface(iface, utils.find_iface_type(iface), "pci", pci_seq, "0", macaddr)
         elif "d0058000.usb" in path:
             # USB on the CPU module
-            append_iface(iface, utils.find_iface_type(iface), "usb", 0, 0, macaddr)
+            append_iface(iface, utils.find_iface_type(iface), "usb", 0, "0", macaddr)
         elif "d005e000.usb" in path:
             # USB2.0 on the MOXTET connector
             # the only option now is USB device on PCI module
             pci_seq = get_module_rank("pci")
-            append_iface(iface, utils.find_iface_type(iface), "pci", pci_seq, 0, macaddr)
+            append_iface(iface, utils.find_iface_type(iface), "pci", pci_seq, "0", macaddr)
         elif "virtual" in path:
             # virtual ifaces (loopback, bridges, ...) - we don't care about these
             pass
